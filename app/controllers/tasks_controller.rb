5c683940c717ff37bcaf95505539e8b2f97fca46
Yu Nakagawa <nakagawayu@nakagawahiroshinoMacBook-Pro.local>
2020-01-30 18:29:47 +0900

    Add useful functions

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,10 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
   def index
-    @tasks = current_user.tasks
+    @tasks = current_user.tasks.recent
   end
 
   def show
-    @task = current_user.tasks.find(params[:id])
   end
 
   def new
@@ -12,7 +12,6 @@ class TasksController < ApplicationController
   end
 
   def edit
-    @task = current_user.tasks.find(params[:id])
   end
 
   def create
@@ -25,15 +24,13 @@ class TasksController < ApplicationController
   end
 
   def update
-    task = current_user.tasks.find(params[:id])
-    task.update!(task_params)
-    redirect_to tasks_path, notice: "タスク「#{task.name}」を更新しました" 
+    @task.update!(task_params)
+    redirect_to tasks_path, notice: "タスク「#{@task.name}」を更新しました" 
   end
 
   def destroy
-    task = current_user.tasks.find(params[:id])
-    task.destroy
-    redirect_to tasks_path, notice: "タスク「#{task.name}」を削除しました" 
+    @task.destroy
+    redirect_to tasks_path, notice: "タスク「#{@task.name}」を削除しました" 
   end
 
   private
@@ -42,4 +39,7 @@ class TasksController < ApplicationController
     params.require(:task).permit(:name, :description)
   end
 
+  def set_task
+    @task = current_user.tasks.find(params[:id])
+  end
 end
